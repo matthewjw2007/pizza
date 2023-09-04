af6959478813d89bf5dcf74dc5631d147e90b26e
Matthew Esqueda <matthewjw2007@yahoo.com>
2023-09-04 14:40:19 -0700

    removed redis piece

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,6 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__, template_folder='./templates', static_folder='./static')
     app.config.from_object(Config)
-    app.task_queue = rq.Queue(connection=app.redis)
     migrate = Migrate(app, db)
     db.init_app(app)
 
